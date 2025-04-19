c18feb0efb5336a553a56e74c1c5ca50e0604e6e
ArnabNandi2008 <136783030+ArnabNandi2008@users.noreply.github.com>
2025-04-19 14:42:49 +0530

    pic added
    lots of bg pic added

---
+++ test.py
@@ -0,0 +1,9 @@
+for i in range(3,244,1) :
+    add = str(i)
+    print(" "*4,"<a target=\"_blank\" href=\"gallery/",add,".jpg\">",sep="")
+    print(" "*6,"<img src=\"gallery/",add,".jpg\" width=\"600\" height=\"400\">",sep="")
+    print(" "*4,"</a>",sep="")
+    
+# 4 <a target="_blank" href="gallery/1.jpg">
+# 6 <img src="gallery/1.jpg" width="600" height="400">
+# 4 </a>
