327a891c76f86747acd4b09b73b369f68830b037
ArnabNandi2008 <136783030+ArnabNandi2008@users.noreply.github.com>
2025-04-21 09:15:22 +0530

    lines added

--- test.py
+++ test.py
@@ -1,9 +1,28 @@
-for i in range(3,4,1) :
-    add = str(i)
-    print(" "*4,"<a target=\"_blank\" href=\"gallery/",add,".jpg\">",sep="")
-    print(" "*6,"<img src=\"gallery/",add,".jpg\" width=\"600\" height=\"400\">",sep="")
-    print(" "*4,"</a>",sep="")
+for i in range(101,230,2) :
+    add1 = str(i)
+    add2 = str(i+1)
+    print(" "*4,"<tr>",sep="")
+    print(" "*6,"<td style=\"width: 50%;\">",sep="")
+    print(" "*8,"<a target=\"_blank\" href=\"gallery/",add1,".jpg\">",sep="")
+    print(" "*10,"<img class=\"gallery\" src=\"gallery/",add1,".jpg\" width=\"600\" height=\"400\">",sep="")
+    print(" "*8,"</a>",sep="")
+    print(" "*6,"</td>",sep="")
+    print(" "*6,"<td style=\"width: 50%;\">",sep="")
+    print(" "*8,"<a target=\"_blank\" href=\"gallery/",add2,".jpg\">",sep="")
+    print(" "*10,"<img class=\"gallery\" src=\"gallery/",add2,".jpg\" width=\"600\" height=\"400\">",sep="")
+    print(" "*8,"</a>",sep="")
+    print(" "*6,"</td>",sep="")
+    print(" "*4,"</tr>",sep="")
     
-# 4 <a target="_blank" href="gallery/1.jpg">
-# 6 <img src="gallery/1.jpg" width="600" height="400">
-# 4 </a>
+#  4  <tr>
+#  6  <td style="width: 50%;">
+#  8  <a target="_blank" href="gallery/3.jpg">
+# 10  <img class="gallery" src="gallery/3.jpg" width="600" height="400">
+#  8  </a>
+#  6  </td>
+#  6  <td style="width: 50%;">
+#  8  <a target="_blank" href="gallery/4.jpg">
+# 10  <img class="gallery" src="gallery/4.jpg" width="600" height="400">
+#  8  </a>
+#  6  </td>
+#  4  </tr>
